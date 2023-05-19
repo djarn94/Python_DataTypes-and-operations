891edb7e38c9574d0f9533ed9bc891708dbf456c
djarn94 <superdjarn@gmail.com>
2023-05-19 22:28:10 +0200

    new tasks

---
+++ comments/main.py
@@ -0,0 +1,22 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+#Global variables
+world = "Grill"
+livability = "unknown" #livability of a world
+worldSafety = False #if the world is safe or not
+
+"""this small code is being used for comment practise.
+Nothing more then that."""
+
+#piece of code that describes if a world is safe or not.
+if worldSafety == True :
+    print("You can live here! but it is a",world,"world")
+else:
+    print("Planet status is",livability,"high chance this world is not safe to live on.")
+
+"""This is the end of the practise,
+I hope you enjoyed. """
---
+++ comments/newfile.py
@@ -0,0 +1,9 @@
+text = "123"
+textNumber = int(text)
+
+print(textNumber + 5)
+
+
+examp = "hello my man \"s\" is this oke?\nit still confuses me a \tbit"
+
+print(examp)
